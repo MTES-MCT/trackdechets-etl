8c76661debf85a3daf2f1b41b4c9bc1cb83efeec
Colin Maudry <colin@maudry.com>
2021-10-04 12:58:33 +0200

    Removed siretisation from gerep/irep, enrich IC_installation_classee with ICPE

--- dags/icpe-siretisation.py
+++ dags/icpe-siretisation.py
@@ -2,13 +2,14 @@ from airflow.decorators import dag, task
 from airflow.exceptions import AirflowFailException
 from airflow.models import Variable
 from datetime import datetime
+from os.path import join
 
 
 @task()
 def initDir() -> str:
     import os
 
-    tmpDataDir: str = Variable.get("TMP_DATA_DIR_BASE") + str(datetime.now()) + '/'
+    tmpDataDir: str = join(Variable.get("TMP_DATA_DIR_BASE"), 'tmp' + str(datetime.now()), '')
     Variable.set('TMP_DATA_DIR', tmpDataDir)
 
     os.mkdir(tmpDataDir)
@@ -20,6 +21,7 @@ def getIrepData(tmpDataDir) -> str:
     import pandas as pd
 
     irepPath = Variable.get("IREP_PATH")
+    irepCsvPath = join(tmpDataDir, "irep.csv")
     df = pd.read_csv(
         irepPath,
         usecols=['identifiant', 'numero_siret'],
@@ -31,39 +33,19 @@ def getIrepData(tmpDataDir) -> str:
     df.rename(columns={'identifiant': 's3ic', 'numero_siret': 'siret'})
     df.drop_duplicates(inplace=True)
     print("Longueur dataframe IREP : " + str(len(df)))
-    df.to_csv(tmpDataDir + "irep.csv")
+    df.to_csv(irepCsvPath)
 
-    return tmpDataDir + "irep.csv"
+    return irepCsvPath
 
-
-@task()
-def getGerepData(tmp_data_dir) -> str:
-    import pandas as pd
-
-    gerepPath = Variable.get("GEREP_PATH")
-    df = pd.read_csv(
-        gerepPath,
-        usecols=['Code établissement', 'Numero Siret'],
-        dtype={
-            'Code établissement': str,
-            'Numero Siret': str
-        })
-    df.rename(columns={'Code établissement': 's3icId', 'Numero Siret': 'siret'}, inplace=True)
-    df.drop_duplicates(inplace=True)
-
-    print("Longueur dataframe GEREP : " + str(len(df)))
-    df.to_csv(tmp_data_dir + "gerep.csv")
-
-    return tmp_data_dir + "gerep.csv"
 
 
 @task()
 def downloadIcpeData(tmp_data_dir) -> str:
     import requests
 
-    icpeTarPath = tmp_data_dir + 'icpe.tar.gz'
+    icpeTarPath = join(tmp_data_dir, 'icpe.tar.gz')
 
-    icpeUrl = Variable.get("ICPE_URL")
+    icpeUrl: str = Variable.get("ICPE_URL")
 
     # If the file is on the local filesystem (testing env), copy instead of downloading
     if icpeUrl.startswith('/'):
@@ -77,137 +59,119 @@ def downloadIcpeData(tmp_data_dir) -> str:
 
 
 @task()
-def extractIcpeFile(icpeTarPath) -> str:
-    import os
+def extractIcpeFiles(icpeTarPath) -> list:
     import tarfile
 
     tmpDataDir = Variable.get('TMP_DATA_DIR')
 
     # https://stackoverflow.com/a/37474942
     tar = tarfile.open(icpeTarPath, 'r:gz')
-    oriFileName = 'IC_etablissement.csv'
-    oriFilePath = tmpDataDir + oriFileName
-    icpePath = tmpDataDir + 'icpe_ori.csv'
+    filesToExtract = [
+        'IC_etablissement.csv',
+        'IC_installation_classee.csv',
+        'IC_ref_nomenclature_ic.csv'
+    ]
     for member in tar.getmembers():
-        if member.name == oriFileName:
+        if member.name in filesToExtract:
             tar.extract(member, path=tmpDataDir)
-            os.rename(oriFilePath, icpePath)
-    if os.path.exists(icpePath) and os.path.getsize(icpePath) > 0:
-        print('ICPE data file extracted successfully.')
-    else:
-        raise AirflowFailException
-    return icpePath
+
+    return filesToExtract
 
 
 @task()
-def addIcpeHeaders(icpePath) -> str:
+def addIcpeHeaders(icpeFiles: list) -> dict:
     import pandas as pd
 
     tmpDataDir = Variable.get('TMP_DATA_DIR')
-    icpe_with_headers = '{}icpe_{}.csv'.format(tmpDataDir, str(datetime.time(datetime.now())))
+    now = str(datetime.time(datetime.now()))
 
-    pd.read_csv(icpePath, sep=';', header=1, dtype={'siret': str, 'codePostal': str, 'codeCommune': str}, names=[
-        's3icId',
-        'siret',
-        'x',
-        'y',
-        'region',
-        'raisonSociale',
-        'codeCommuneEtablissement',
-        'codePostal',
-        # 1 = en construction, 2 = en fonctionnement, 3 = à l'arrêt, 4 = cessation déclarée, 5 = Récolement fait
-        'etatActivite',
-        'codeApe',
-        'nomCommune',
-        'seveso',
-        'regime',
-        'prioriteNationale',
-        # cf. biblio https://aida.ineris.fr/node/193
-        'ippc',
-        # Etablissement soumis à la déclaration annuelle d'émissions polluantes et de déchets
-        'declarationAnnuelle',
-        # IN = industrie, BO = bovins, PO = porcs, VO = volailles, CA = carrières
-        'familleIc',
-        # 1 + 1 = DREAL, etc.
-        'baseIdService',
-        'natureIdService',
-        'adresse1',
-        'adresse2',
-        'dateInspection',
-        # Sites et sols pollués:
-        'indicationSsp',
-        'rayon',
-        'precisionPositionnement'
-    ]).to_csv(icpe_with_headers, sep=',')
+    def makeNewFilename(ori) -> str:
+        return join(tmpDataDir, '{}_{}.csv'.format(ori, now))
 
-    return icpe_with_headers
-
-
-@task()
-def siretisationIcpe(icpePath, irepPath, gerepPath) -> str:
-    import pandas as pd
-
-    tmpDataDir = Variable.get('TMP_DATA_DIR')
-    sirene_path = Variable.get("SIRENE_PATH")
-    print(sirene_path)
-
-    icpe = pd.read_csv(
-        icpePath,
-        keep_default_na=False,
-        index_col=['siret', 'codeCommuneEtablissement']
-    )
-
-    # We tag the icpe rows that have valid SIRET
-    icpe['siret_valid'] = icpe['siret'].str.len() == 14
-    # and keep only the valid ones
-    icpe = icpe.loc[icpe['siret_valid']]
-
-
-    # Le fichier StockEtablissement (https://www.data.gouv.fr/fr/datasets/base-sirene-des-entreprises-et-de-leurs-etablissements-siren-siret/)
-    # avec au moins les colonnes suivantes :
-    # - siret
-    # - codePostalEtablissement
-    # - etatAdministratifEtablissement
-    # - libelleCommuneEtablissement
-    # - numeroVoieEtablissement
-    # - typeVoieEtablissement
-    # - activitePrincipaleEtablissement
-    # - libelleVoieEtablissement
-
-    with pd.read_csv(
-        sirene_path,
-        low_memory=True,
-        chunksize=3000000,
-        usecols=['siret', 'codePostalEtablissement', 'etatAdministratifEtablissement', 'codeCommuneEtablissement', 'libelleCommuneEtablissement',
-                 'numeroVoieEtablissement', 'typeVoieEtablissement', 'activitePrincipaleEtablissement',
-                 'libelleVoieEtablissement'],
-        dtype={
-            'siret': str,
-            'codePostalEtablissement': str,
-            'etatAdministratifEtablissement': 'category',
-            'codeCommuneEtablissement': str,
-            'libelleCommuneEtablissement': str,
-            'numeroVoieEtablissement': str,
-            'typeVoieEtablissement': 'category',
-            'activitePrincipaleEtablissement': 'category',
-            'libelleVoieEtablissement': str
+    options = {
+        "IC_etablissement.csv": {
+            'names': [
+                's3icId',
+                'siret',
+                'x', 'y', 'region', 'raisonSociale', 'codeCommuneEtablissement', 'codePostal',
+                # 1 = en construction, 2 = en fonctionnement, 3 = à l'arrêt, 4 = cessation déclarée, 5 = Récolement fait
+                'etatActivite',
+                'codeApe', 'nomCommune', 'seveso', 'regime', 'prioriteNationale',
+                # cf. biblio https://aida.ineris.fr/node/193
+                'ippc',
+                # Etablissement soumis à la déclaration annuelle d'émissions polluantes et de déchets
+                'declarationAnnuelle',
+                # IN = industrie, BO = bovins, PO = porcs, VO = volailles, CA = carrières
+                'familleIc',
+                # 1 + 1 = DREAL, etc.
+                'baseIdService',
+                'natureIdService', 'adresse1' 'adresse2', 'dateInspection',
+                # Sites et sols pollués:
+                'indicationSsp',
+                'rayon', 'precisionPositionnement'
+            ],
+            'dtype': {'siret': str, 'codePostal': str, 'codeCommune': str},
+            'parse_dates': []
         },
-        index_col=['siret', 'codeCommuneEtablissement']) as reader:
+        'IC_installation_classee.csv': {
+            'names': [
+                's3icId', 'id_installation_classee', 'volume', 'unite', 'date_debut_exploitation',  'date_fin_validite', 'statut_ic', 'id_ref_nomencla_ic'
+            ],
+            'dtype': {
+                's3icId': str, 'volume': float, 'statut_ic': str
+            },
+            'parse_dates': ['date_debut_exploitation', 'date_fin_validite']
+        },
+        'IC_ref_nomenclature_ic.csv': {
+            'names': [
+                'id', 'rubrique_ic', 'famille_ic', 'sfamille_ic', 'ssfamille_ic', 'alinea', 'libellecourt_activite', 'id_regime', 'envigueur', 'ippc'
+            ],
+            'dtype': {
+                'rubrique_ic': str,
+                'alinea': str,
+                'id_regime': str,
+                'envigueur': int,
+                'ippc': int
+            },
+            'parse_dates': []
+        }
 
-        for chunk in reader:
-            pass
+    }
+
+    icpeWithHeaders = {}
+
+    for file in icpeFiles:
+        newFilename = join(tmpDataDir, makeNewFilename(file))
+        icpeWithHeaders[file] = newFilename
+        pd.read_csv(join(tmpDataDir, file), sep=';', header=1, dtype=options[file]['dtype'], parse_dates=options[file]['parse_dates'],
+                    names=options[file]['names']) \
+            .to_csv(newFilename, sep=',')
+
+    return icpeWithHeaders
 
 
-    icpe_siretisation = '{}icpe_{}.csv'.format(tmpDataDir, str(datetime.time(datetime.now())))
-    icpe.to_csv(icpe_siretisation)
+@task()
+def enrichInstallations(icpeFiles: dict, irepFile) -> str:
+    import pandas as pd
 
-    return icpe_siretisation
+    tmpDataDir = Variable.get('TMP_DATA_DIR')
+
+    ic_siretise = join(tmpDataDir, 'ic_siretise.csv')
+
+    etablissements = pd.read_csv(icpeFiles['IC_etablissement.csv'], index_col='s3icId', usecols=['s3icId', 'siret'])
+    installations = pd.read_csv(icpeFiles['IC_installation_classee.csv'], index_col='id_installation_classee')
+
+    installations = installations.join(etablissements)
+
+    installations.to_csv(ic_siretise)
+
+    return ic_siretise
 
 
 @task()
 def siretisationStats(siretisation_path):
     import pandas as pd
-    icpe = pd.read_csv(siretisation_path, dtype={'siret': str}, keep_default_na=False)
+    icpe = pd.read_csv(siretisation_path, dtype={'siret': str}, keep_default_na=False, usecols=['siret'])
     end_with_zero = len(icpe.loc[icpe['siret'].str.endswith('00000')].index)
     empty_siret = len(icpe.loc[icpe['siret'] == ''].index)
     total = len(icpe.index)
@@ -220,21 +184,28 @@ def siretisationStats(siretisation_path):
     return stats
 
 
+@task()
+def loadToDatabase():
+
+    # 1 table for installations
+    # 1 table for rubriques
+    pass
+
+
+
+
 @dag(start_date=datetime(2021, 1, 1),
      schedule_interval=None,
      user_defined_macros={},
      catchup=False)
-def icpeSiretisation():
+def icpeETL():
     init_dir = initDir()
-    get_gerep_data = getGerepData(init_dir)
     get_irep_data = getIrepData(init_dir)
     download_icpe_data = downloadIcpeData(init_dir)
-    get_icpe_data = extractIcpeFile(download_icpe_data)
+    get_icpe_data = extractIcpeFiles(download_icpe_data)
     add_icpe_headers = addIcpeHeaders(get_icpe_data)
-    icpe_siretise = siretisationIcpe(add_icpe_headers, get_irep_data, get_gerep_data)
-    siretisationStats(icpe_siretise)
+    ic_siretise = enrichInstallations(add_icpe_headers, get_irep_data)
+    siretisationStats(ic_siretise)
 
 
-
-
-icpe_siretisation_etl = icpeSiretisation()
+icpe_etl = icpeETL()

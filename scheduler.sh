c29bb263e603c4f2b0a3dfbb7c0f752bce401621
Colin Maudry <colin@maudry.com>
2021-10-12 14:03:50 +0200

    Removed sleep from script

--- scheduler.sh
+++ scheduler.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # start the scheduler
 # open a new terminal or else run webserver with ``-D`` option to run it as a daemon
-echo "Wait for init..."
-sleep 5
+
 echo "Starting the scheduler..."
 airflow scheduler

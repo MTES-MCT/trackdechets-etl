41b562105f554c5b792bca0aecf4daef7bf11b25
Colin Maudry <colin@maudry.com>
2021-10-12 14:03:28 +0200

    Removed unused variables (IREP, GEREP, SIRENE...)

--- sample.env.sh
+++ sample.env.sh
@@ -32,10 +32,6 @@ DATA_ROOT_DIR="/media/colin/data"
 
 # Data sources
 export AIRFLOW_VAR_ICPE_URL="${DATA_ROOT_DIR}/georisques-icpe/S3IC-Georisques.tar.gz"
-export AIRFLOW_VAR_GEREP_PATH="${DATA_ROOT_DIR}/georisques-gerep/gerep-donnees-2016-2017-fusion.csv"
-export AIRFLOW_VAR_IREP_PATH="${DATA_ROOT_DIR}/georisques-irep/etablissements-2019.csv"
-export AIRFLOW_VAR_SIRENE_PATH="${DATA_ROOT_DIR}/sirene/StockEtablissement_utf8.csv"
-export AIRFLOW_VAR_TMP_DATA_DIR_BASE="/home/colin/git/trackdechets-etl"
 
 # Airflow settings override
 export AIRFLOW__CORE__LOAD_EXAMPLES=False

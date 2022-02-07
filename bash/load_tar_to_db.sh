2fece208f34117b665b9772e924f2ffdc3d89cc4
Colin Maudry <colin@maudry.com>
2022-02-07 14:43:40 +0100

    Removed bash file

--- bash/load_tar_to_db.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-tar=tarfile.tar.gz
-
-DATABASE_URL="fiche_inspe_4015:IdwOESjNz1nmX5NkeGIY@locahost:10000/fiche_inspe_4015"
-
-pg_restore --clean --if-exists --no-owner --no-privileges  --dbname $DATABASE_URL "tarfile/20211122000001_trackdechet_8889.pgsql"
-

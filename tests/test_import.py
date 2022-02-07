a75a272845a52e2ea1507b6cfb8fb995dffc2817
Colin Maudry <colin@maudry.com>
2022-02-07 14:42:23 +0100

    New line at end of Pipfile and test script

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,6 +1,7 @@
 import pytest
 from airflow.models import DagBag
 
+
 def test_no_import_errors():
-  dag_bag = DagBag()
-  assert len(dag_bag.import_errors) == 0, "No Import Failures"
+    dag_bag = DagBag()
+    assert len(dag_bag.import_errors) == 0, "No Import Failures"
